55faa1dc2b7c7075a5cf71a272fbaf8232241ab3
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2025-05-14 21:59:29 +0330

    Make Tags and News models and migrate

--- tasknews/news/models.py
+++ tasknews/news/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Tags(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Tag"
+        verbose_name_plural = "Tags"
+
+
+class News(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    source = models.CharField(max_length=200)
+    tags = models.ManyToManyField(Tags, related_name="news_items")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "New"
+        verbose_name_plural = "News"
