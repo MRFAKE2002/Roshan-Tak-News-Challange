f08328ca678fe9d30f9273409d94b66a8b102418
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2025-05-14 23:23:30 +0330

    Add filter in NewsList view

--- tasknews/news/views.py
+++ tasknews/news/views.py
@@ -1,9 +1,11 @@
 # Libraries
 from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
 
 # My Apps
 from .models import News
 from .serializers import NewsSerializers
+from .filters import NewsFilter
 
 
 # ---------------------------------------< News List >----------------------------------- #
@@ -12,3 +14,6 @@ from .serializers import NewsSerializers
 class NewsList(generics.ListAPIView):
     queryset = News.objects.prefetch_related("tags").all().order_by("-created_at")
     serializer_class = NewsSerializers
+
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = NewsFilter
