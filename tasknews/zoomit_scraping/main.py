ac7f8c11b278dd19e78c2acb78a1b644de4e535d
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2025-05-17 13:37:07 +0330

    Make main.py to run scraper and save data in DataBase

---
+++ tasknews/zoomit_scraping/main.py
@@ -0,0 +1,58 @@
+# Django
+import django
+
+# Libraries
+import os
+import sys
+import pathlib
+
+
+sys.path.append(str(pathlib.Path(__file__).resolve().parent.parent))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tasknews.settings")
+django.setup()
+
+# My Apps
+from zoomit_scraping.scraper import get_article_links, extract_article_data
+from news.models import News, Tags
+
+
+def main():
+    article_links = get_article_links()
+    print(f"There is {len(article_links)} articles here.")
+    print("-----------------------------------")
+
+    for i, link in enumerate(article_links[:5], start=1):
+        print(f"\n {i}: {link}")
+
+        data = extract_article_data(link)
+
+        if data:
+            print(f"title: {data['title']}")
+            print(f"tags: {', '.join(data['tags'])}")
+            print(
+                f"paragraphs: {data['paragraphs'][0] if data['paragraphs'] else 'There is no paragraphs.'}"
+            )
+
+            if not News.objects.filter(source=data["url"]).exists():
+                news_item = News.objects.create(
+                    title=data["title"],
+                    content="\n\n".join(data["paragraphs"]),
+                    source=data["url"],
+                )
+
+                for tag_name in data["tags"]:
+                    tag_obj, created = Tags.objects.get_or_create(name=tag_name)
+                    news_item.tags.add(tag_obj)
+
+                print("Article saved successfully.")
+            else:
+
+                print("Article already exist. Skipping...")
+
+        else:
+            print("[!] There is error about article data !!!")
+
+
+if __name__ == "__main__":
+    main()
